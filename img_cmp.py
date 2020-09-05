6c35bacfe0a7900318dc6ef0087265daf907da61
MetLee <met@whu.edu.cn>
2020-09-05 14:39:25 +0800

    Unicode escape
    unquote不能转义%u

--- img_cmp.py
+++ img_cmp.py
@@ -10,7 +10,7 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 def unquote_u(string: str):
     result = unquote(string, encoding='utf-8', errors='replace')
     if '%u' in result:
-        result = result.replace('%u', '\\u').encode("utf-8").decode('unicode_escape')
+        result = result.replace('%u', '\\u').encode('utf-8').decode('unicode_escape')
     return result
 
 
