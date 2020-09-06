71716664d12829222b01ad8087573e222971a3cd
Charles <Charles@0x7f.cc>
2020-09-06 11:15:32 +0800

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,17 +1,29 @@
 import shutil
-
+import glob
 import os
 
 def main():
     if input('Do you want to clean generated data? (y/n)') == 'y':
-        shutil.rmtree('cmp')
-        os.remove('result_pre.jpg')
-        os.remove('result.jpg')
-        os.remove('result_full.jpg')
+        if os.path.isdir('cmp'):
+            shutil.rmtree('cmp')
+        if os.path.exists('result_pre.jpg'):
+            os.remove('result_pre.jpg')
+        if os.path.exists('result.jpg'):
+            os.remove('result.jpg')
+        if os.path.exists('result_full.jpg'):
+            os.remove('result_full.jpg')
+        if os.path.exists('result.json'):
+            os.remove('result.json')
+        fileList = glob.glob('result_*.jpg')
+        for filePath in fileList:
+            os.remove(filePath)
     if input('Do you want to clean portals dataset? (y/n)') == 'y':
-        os.rmtree('data')
-        os.rmtree('data_feature')
-        os.rmtree('data_feature_preview')
+        if os.path.isdir('data'):
+            os.rmtree('data')
+        if os.path.isdir('data_feature'):
+            os.rmtree('data_feature')
+        if os.path.isdir('data_feature_preview'):
+            os.rmtree('data_feature_preview')
 
 if __name__ == "__main__":
     main()
